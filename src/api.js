e7284b1419f3ee13de2f9e000c93c36f9613e4d6
Aditya M <52854951+adityam945@users.noreply.github.com>
2020-06-03 22:30:14 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
+  res.json([{
     subcode: "17CS46",
     subname: "DATA COMMUNICATION ",
     m1: "Module 1",
@@ -26,7 +26,7 @@ router.get("/", (req, res) => {
     descname: "Prescribed text books",
     descdata:
       "Text  Book:\n Behrouz A. Forouzan, Data Communications and Networking 5E, 5th Edition, Tata McGraw-Hill, 2013. (Chapters 1.1 to 1.5, 2.1 to 2.3, 3.1, 3.3 to 3.6, 4.1 to 4.3, 5.1, 6.1, 6.2, 8.1 to 8.3, 10.1 to 10.5, 11.1 to 11.4, 12.1 to 12.3, 13.1 to 13.5, 15.1 to 15.3, 16.1 to 16.3, 19.1 to 19.3, 22.1 to 22.4)\n\n Reference Books:\n 1. Alberto Leon-Garcia and Indra Widjaja: Communication Networks - Fundamental Concepts and Key architectures, 2nd Edition Tata McGraw-Hill, 2004. \n 2. William Stallings: Data and Computer Communication, 8th Edition, Pearson Education, 2007.\n  3. Larry L. Peterson and Bruce S. Davie: Computer Networks – A Systems Approach, 4th   Edition, Elsevier, 2007. \n 4. Nader F. Mir: Computer and Communication Networks, Pearson Education, 2007 ",
-  });
+  }]);
 });
 
 app.use(`/.netlify/functions/api`, router);
